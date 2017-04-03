e2a812ba934a188dbda525f6d9d7beac1fff0e94
Sabrina Fritz <safritz@techfak.uni-bielefeld.de>
2017-04-03 16:33:51 +0200

    add example app

---
+++ AndroidVertiefung/app/src/main/java/com/example/sabrinafritz/androidvertiefung/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.example.sabrinafritz.androidvertiefung;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sabrinafritz on 14.03.17.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    /**
+     * load the fragment layout.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+    }
+}
