0c61f54997b02a19af2f7a2be069c91d147b188d
Sabrina Fritz <safritz@techfak.uni-bielefeld.de>
2017-04-03 17:12:30 +0200

    change returnvalue mainfragment

--- AndroidVertiefung/app/src/main/java/com/example/sabrinafritz/androidvertiefung/MainActivityFragment.java
+++ AndroidVertiefung/app/src/main/java/com/example/sabrinafritz/androidvertiefung/MainActivityFragment.java
@@ -21,7 +21,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
 
     }
 }
